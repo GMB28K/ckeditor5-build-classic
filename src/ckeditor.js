4fc22aa458e3f4a016a017c31ea38bc99e0bd8e4
Victor A <victor@namespace.ee>
2020-04-25 14:17:48 +0300

    add custom plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,12 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +62,12 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	CodeBlock,
+	ImageResize,
+	Mention,
+	WordCount,
+	SimpleUploadAdapter
 ];
 
 // Editor configuration.
